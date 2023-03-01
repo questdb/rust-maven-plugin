4f3976f96feb9d90708f5e921d463669e69ee4d8
Adam Cimarosti <cimarosti@gmail.com>
2023-03-01 16:31:56 +0000

    fix: Adding support for 'skipTests' in the rust-maven-plugin test mojo.

--- rust-maven-plugin/src/main/java/io/questdb/maven/rust/CargoTestMojo.java
+++ rust-maven-plugin/src/main/java/io/questdb/maven/rust/CargoTestMojo.java
@@ -28,11 +28,22 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 
 @Mojo(name = "test", defaultPhase = LifecyclePhase.TEST, threadSafe = true)
 public class CargoTestMojo extends CargoMojoBase {
+    /**
+     * Skips running tests with building with `mvn package -DskipTests=true`.
+     */
+    @Parameter(property = "skipTests", defaultValue = "false")
+    private boolean skipTests;
+
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
+        if (skipTests) {
+            getLog().info("Skipping tests");
+            return;
+        }
         final Crate crate = new Crate(
                 getCrateRoot(),
                 getTargetRootDir(),
